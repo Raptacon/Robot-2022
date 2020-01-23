db775aa7e202a7e958be46dc82169517beeb7af6
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2020-01-22 20:16:42 -0700

    Set up basic structure for driveTrain component

---
+++ components/driveTrain.py
@@ -0,0 +1,31 @@
+import wpilib
+
+class driveTrain:
+    leftMotors: leftMotorsList
+    rightMotors: rightMotorsList
+
+    def __init__(self):            
+    
+    def getLeft():
+        pass
+
+    def getRight():
+        pass
+
+    def isStopping():
+        pass
+
+    def setArcade(speed, rotation):
+        pass
+    
+    def setTank(leftSpeed, rightSpeed):
+        pass
+
+    def stop(coast = False):
+        pass
+
+    def getMeasuredSpeed():
+        pass
+
+    def execute():
+        pass
