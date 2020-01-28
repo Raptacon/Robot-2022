e9af3d282a4db1b29cf4e2ac1a25644618ccc77a
Nathan Beasley <tuffinmuffin@gmail.com>
2020-01-27 22:34:17 -0700

    Revert "test code for west coast drive. Very dirty pleast don't reuse" Commited to wrong branch This reverts commit 7a7ae5c8c3c80280cdebb5f84eb7b396d7c427df.

--- robot.py
+++ robot.py
@@ -3,11 +3,7 @@ Base file that setups basic robot. Actual robot is in team3200 module.
 This file should not need to be edited.
 """
 import wpilib
-import team3200
-
-class Robot(team3200.Team3200Robot):
-    pass
-
+from team3200 import Robot as Robot
 
 if __name__ == '__main__':
     try:
---
+++ team3200/robot.py
@@ -0,0 +1,9 @@
+#import team3200
+import commandbased
+
+
+class Robot(commandbased.CommandBasedRobot):
+
+    def robotInit(self):
+        '''This is where the robot code starts.'''
+        pass
