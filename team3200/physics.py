365c5b7f8e328086ded0ef27e719217047299f91
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2020-01-31 17:59:41 -0700

    fixed some of the flake8 issues, looking to rerun pipelines

--- team3200/physics.py
+++ team3200/physics.py
@@ -8,33 +8,33 @@ from pyfrc.physics import drivetrains
 
 class PhysicsEngine(object):
     '''
-    Simulates a 4-wheel mecanum robot using Tank Drive joystick control 
+    Simulates a 4-wheel mecanum robot using Tank Drive joystick control
     '''
-    
+
     def __init__(self, physics_controller):
         '''
             :param physics_controller: `pyfrc.physics.core.Physics` object
         to communicate simulation effects to
         '''
-        
+
         self.physics_controller = physics_controller
-        
-            
+
+
     def update_sim(self, hal_data, now, tm_diff):
         '''
             Called when the simulation parameters for the program need to be
             updated.
-            
+
             :param now: The current time as a float
             :param tm_diff: The amount of time that has passed since the last
                             time that this function was called
         '''
-        
+
         # Simulate the drivetrain
         # -> Remember, in the constructor we inverted the left motors, so
         #    invert the motor values here too!
-        try: 
-            
+        try:
+
             if hal_data['CAN'][1]['inverted']:
                 l_motor = -hal_data['CAN'][1]['value']
             else:
@@ -51,6 +51,6 @@ class PhysicsEngine(object):
         except:
             l_motor = r_motor = 0
             self.physics_controller.drive(0,0, tm_diff)
-        
+
         #print(hal_data['joysticks'])
         #.mecanum_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor,speed = 0.01)
