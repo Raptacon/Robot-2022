85c760cf05b8424a58baabf99cb7c6a4636e75fb
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2020-02-12 19:04:09 -0700

    Changed to use path directory instead of hardcoded if statements

--- robotMap.py
+++ robotMap.py
@@ -1,5 +1,5 @@
 import ConfigMapper as mapper
-import platform
+import os
 
 class RobotMap():
     """
@@ -8,13 +8,8 @@ class RobotMap():
     """
     def __init__(self):
         """intilize the robot map"""
-        configFileName = "config.yml" #Put filename for config here, should be in the same directory as robotMap.py
-
-
-        if platform.system() == "Linux": # added to accomodate the bot - It doesn't run with just "config.yml". I imagine that there's a better way to do this, either finding if you're on the roborio or just working around it
-            config = mapper.ConfigMapper("/home/lvuser/py/" + configFileName)
-        else:
-            config = mapper.ConfigMapper(configFileName)
+        configFile = os.path.dirname(__file__) + os.path.sep + "config.yml" #Put filename for config here, should be in the same directory as robot.py
+        config = mapper.ConfigMapper(configFile)
 
         self.motorsMap = CANMap(config)
 
