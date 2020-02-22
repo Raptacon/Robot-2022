1b81ae4ea90610b16722a138570e8ceb928ca2ff
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2020-02-21 17:46:44 -0700

    added compressor

--- components/pneumatics.py
+++ components/pneumatics.py
@@ -4,6 +4,8 @@ class Pneumatics:
     
     def setup(self):
         self.solenoid = wpilib.DoubleSolenoid(0, 1) #I don't know if this is the proper a) class to define or b) the right channels for the solenoid. This is mostly a placeholder and can be fixed
+        self.compressor = wpilib.compressor()
+        self.compressor.start()
 
     def get(self):
         return self.solenoid.get()
