d8d405e291d11ff6881d0f013f93550331c18b38
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2020-02-21 18:48:55 -0700

    Added doc strings and notes

--- components/pneumatics.py
+++ components/pneumatics.py
@@ -3,6 +3,10 @@ import wpilib
 class Pneumatics:
     
     def setup(self):
+        """
+        Setup to enable everything after variable injection from robot.py. This is where the bulk of setup for this class should be. 
+        on_enable() may need to be used for when something needs to happen everytime the state is changed, like from autonomous to teleop. 
+        """
         self.solenoid = wpilib.DoubleSolenoid(0, 1) #I don't know if this is the proper a) class to define or b) the right channels for the solenoid. This is mostly a placeholder and can be fixed
         self.compressor = wpilib.Compressor()
         self.compressor.start()
@@ -14,10 +18,25 @@ class Pneumatics:
         return self.solenoid.get()
         
     def enableSolenoid(self):
-        self.solenoid.set(True)
+        """
+        Turn the solenoid into the "on" position. This can vary per configuration
+        """
+        self.solenoid.set(True) #currently, this is only set to handle one solenoid. I believe that both bots only have one.
 
     def disableSolenoid(self):
-        self.solenoid.set(False)
+        """
+        Turn the solenoid into the "off" position. This can vary per configuration
+        """
+        self.solenoid.set(False) #currently, this is only set to handle one solenoid. I believe that both bots only have one.
+
+    def getCompressorCurrent(self):
+        """
+        Returns how much power the compressor is currently drawing. Useful to not brown out
+        """
+        return self.compressor.getCompressorCurrent()
 
     def execute(self):
+        """
+        Nothing is needed to run every period. Otherwise, this would have something other than pass
+        """
         pass
