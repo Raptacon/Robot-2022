511e59d45fa844bc949ef16e8a73258208fb5c2f
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2020-02-21 18:19:18 -0700

    capitalization

--- components/pneumatics.py
+++ components/pneumatics.py
@@ -4,11 +4,20 @@ class Pneumatics:
     
     def setup(self):
         self.solenoid = wpilib.DoubleSolenoid(0, 1) #I don't know if this is the proper a) class to define or b) the right channels for the solenoid. This is mostly a placeholder and can be fixed
-        self.compressor = wpilib.compressor()
+        self.compressor = wpilib.Compressor()
         self.compressor.start()
 
-    def get(self):
+    def getSolenoid(self):
+        """
+        returns the "value" of the solenoid. Boolean, is it on or off?
+        """
         return self.solenoid.get()
         
+    def enableSolenoid(self):
+        self.solenoid.set(True)
+
+    def disableSolenoid(self):
+        self.solenoid.set(False)
+
     def execute(self):
         pass
