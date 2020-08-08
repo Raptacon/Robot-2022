0f6e3901b3d218d7c698e1e177042f55725f1a1f
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2020-08-08 12:39:40 -0600

    Test code (make sure network tables work) and fix things (use the getNumber() method to get numbers from the network talbes) Also added the check for a valid target

--- components/autoAim.py
+++ components/autoAim.py
@@ -1,12 +1,12 @@
 from networktables import NetworkTables as networktable
-from magicbot import AutonomousStateMachine, tunable
+from magicbot import StateMachine, tunable
 from magicbot.state_machine import state, timed_state
 from components.driveTrain import DriveTrain
 from components.shooterLogic import ShooterLogic
 
 
 
-class AutoAim(AutonomousStateMachine):
+class AutoAim(StateMachine):
     compatString = ["doof"]
     time = 0.1
     driveTrain: DriveTrain
@@ -17,17 +17,17 @@ class AutoAim(AutonomousStateMachine):
 
     @state(first = True)
     def start(self):
-        """should add check if limelight even sees it to auto send it to stop in case hit or pressed accidentally"""
-        networktable.getTable("limelight")
-        tx = table("tx")
-        if tx > minAimOffset:
-            self.next_state_now("adjust_self_left")
-        elif tx < -1*minAimOffset:
-           self.next_state_now("adjust_self_right")
-        elif tx < minAimOffset and tx > -1*minAimOffset:
-            self.next_state_now("stop_shoot")
-        else:
-            self.next_state_now("stop")
+        table = networktable.getTable("limelight")
+        if table.getNumber("tv") == 1: #If limelight has any valid targets
+            tx = table.getNumber("tx")
+            if tx > minAimOffset:
+                self.next_state_now("adjust_self_left")
+            elif tx < -1*minAimOffset:
+                self.next_state_now("adjust_self_right")
+            elif tx < minAimOffset and tx > -1*minAimOffset:
+                self.next_state_now("stop_shoot")
+            else:
+                self.next_state_now("stop")
 
 
     @timed_state(duration = time, next_state = "start")
