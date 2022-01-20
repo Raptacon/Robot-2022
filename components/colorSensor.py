7a9d165d5a713d25b5bcd2ec30aaf0ce9f7c75c8
hell0w0rld2 <43834630+hell0w0rld2@users.noreply.github.com>
2022-01-19 19:37:22 -0700

    Tuning and adds an easier way to see the color

--- components/colorSensor.py
+++ components/colorSensor.py
@@ -8,12 +8,12 @@ class ColorSensor():
     colorMatch = ColorMatch()
     # The closer this is to 0, the closer the color has to be to the color.
     confidence = tunable(.1)
-    colors = {"red":wpilib.Color(.5, 0, 0),
-             "blue":wpilib.Color(0, 0, .5),
-             "white":wpilib.Color(1, 1, 1),
+    colors = {"red":wpilib.Color(.4, .15, .05),
+             "blue":wpilib.Color(.1, .2, .4),
+             "white":wpilib.Color(.5, .5, .5),
              "black":wpilib.Color(0, 0, 0)}
+    colorMatched = None
     
-
     def setup(self):
         for color in self.colors.keys():
             self.colorMatch.addColorMatch(self.colors[color])
@@ -38,6 +38,25 @@ class ColorSensor():
 
     def getBlue(self):
         return True if self.colorMatch.matchClosestColor(self.color, self.confidence) == self.colors["blue"] else False
+
+    def getWhite(self):
+        return True if self.colorMatch.matchClosestColor(self.color, self.confidence) == self.colors["white"] else False
+    
+    def getBlack(self):
+        return True if self.colorMatch.matchClosestColor(self.color, self.confidence) == self.colors["black"] else False
+
+    @feedback
+    def displayColor(self):
+        if self.getRed():
+            self.colorMatched = "red"
+        elif self.getBlue():
+            self.colorMatched = "blue"
+        elif self.getWhite():
+            self.colorMatched = "white"
+        elif self.getBlack():
+            self.colorMatched = "black"
+        else:
+            self.colorMatched = "none"
 
     @feedback
     def displayRed(self):
