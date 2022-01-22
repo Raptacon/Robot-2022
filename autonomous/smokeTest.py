6002f394f9b9037d0c2b5fc504bf34d9b74cb62f
hell0w0rld2 <43834630+hell0w0rld2@users.noreply.github.com>
2022-01-21 19:43:11 -0700

    Sets up more states to check other components

--- autonomous/smokeTest.py
+++ autonomous/smokeTest.py
@@ -17,6 +17,7 @@ class SmokeTest(AutonomousStateMachine):
 
     @state(first = True)
     def drive(self):
+        """Tests to see if the motors are working with an input from the driver"""
         self.toDo = "Drive motors forwards"
         if self.driveTrain.tankLeftSpeed > 0 and self.driveTrain.tankRightSpeed > 0:
             self.next_state("colorSensorCheck")
@@ -25,6 +26,25 @@ class SmokeTest(AutonomousStateMachine):
     
     @state
     def colorSensorCheck(self):
+        """Sets the LED on the color sensor to strobe and requires user input to advance to the next state"""
         self.toDo = "Check to see if color sensor LED is flashing"
         self.colorSensor.colorSensor.LEDPulseFrequency(value = 60)
-        
+
+    @state
+    def checkGoToDist(self):
+        """Checks to see if goToDist is working by making it drive forwards a certain distance"""
+        pass
+
+    @state
+    def checkTurnToAngle(self):
+        """Checks to see if turnToAngle is working by making it turn to a certain angle"""
+        pass
+
+    @state
+    def checkEncoders(self):
+        pass
+
+    @state
+    def checkBreakSensors(self):
+        """Create one for each break sensor, """
+        pass
