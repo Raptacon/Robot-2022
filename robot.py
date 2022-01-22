0646b8c0be659d2e80149bff7755a50eb7bf879a
unknown <iankeitlen@gmail.com>
2022-01-22 11:45:51 -0700

    Experimental code for collecting component names

--- robot.py
+++ robot.py
@@ -30,6 +30,23 @@ from components.driveTrainGoToDist import GoToDist
 from components.ballCounter import BallCounter
 from components.colorSensor import ColorSensor
 
+
+import os, sys
+
+dir_path = os.path.dirname(os.path.abspath(__file__), "components")
+files_in_dir = [f[:-3] for f in os.listdir(dir_path)
+                if f.endswith('.py') and f != '__init__.py']
+for f in files_in_dir:
+    mod = __import__('.'.join([__name__, f]), fromlist=[f])
+    to_import = [getattr(mod, x) for x in dir(mod)]
+               # if isinstance(getattr(mod, x), type)]  # if you need classes only
+
+    for i in to_import:
+        try:
+            setattr(sys.modules[__name__], i.__name__, i)
+        except AttributeError:
+            pass
+
 # Other imports:
 from robotMap import RobotMap, XboxMap
 from networktables import NetworkTables
