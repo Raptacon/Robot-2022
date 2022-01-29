03a45ac53b53742ea844db1711cb2345dbca81d1
Sai Koppisetty <skoppisetty@cherrycreekschools.org>
2022-01-29 10:19:35 -0700

    Refining drive_backwards

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -159,8 +159,7 @@ class AutonomousAutoStart(AutonomousStateMachine):
     def drive_backwards(self):
         """Drives the bot backwards for 5 feet"""
         self.goToDist.engage()
-        self.goToDist.setTargetDist(60)
-        self.goToDist.start()
+        self.goToDist.start(60)
         self.StopRunningFirstCall = True
         self.next_state("stoprunning2")
 
