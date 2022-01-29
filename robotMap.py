748facf4cc2f5fd3289fc52225f27b47ec4dd213
IanDKsemantic <iankeitlen@gmail.com>
2022-01-29 11:21:22 -0700

    fixed z/twist axis

--- robotMap.py
+++ robotMap.py
@@ -112,8 +112,7 @@ class JoystickMap():
     def JoystickInput(self):
         self.driveX = self.drive.getX()
         self.driveY = self.drive.getY()
-        self.driveZ = self.drive.getZ()      
-        self.twist = self.drive.getTwist()  
+        self.driveZ = self.drive.getZ()        
         self.trigger = Joystick.getTrigger()
         self.button2 = GenericHID.getRawButton(2)
         self.button3 = GenericHID.getRawButton(3)
@@ -138,9 +137,6 @@ class JoystickMap():
 
     def getDriveZAxis(self): 
         return self.driveZ
-
-    def getTwist(self):
-        return self.twist
 
     def getTrigger(self):
         return self.trigger
