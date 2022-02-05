6f88a0bf1f4540ae394728cc7a76477a0ed9e83b
IanDKsemantic <iankeitlen@gmail.com>
2022-02-05 10:56:44 -0700

    renamed get Position to Turret Threshold

--- components/Actuators/HighLevel/TurretsTurn.py
+++ components/Actuators/HighLevel/TurretsTurn.py
@@ -1,21 +1,21 @@
 from webbrowser import get
 from magicbot import state_machine, state, timed_state, tunable
 from components.Actuators.AutonomousControl.turnToAngle import TurnToAngle
-from components.Actuators.LowLevel.turretThreshold import getPosition
+from components.Actuators.LowLevel.turretThreshold import turretThreshold
 
 class turretTurn:
     compatString = ["doof", "greenChassis"]
     motors_turret: dict
-    GetPosition: getPosition
+    TurretThreshold: turretThreshold
     turnAngle = 0
     tolerance = tunable(0.5)
 
     def on_enable(self):
         self.turretMotor = self.motors_turret["turretMotor"]
-        self.GetPosition.setup()
+        self.TurretThreshold.setup()
 
     def setAngle(self, angle):
-        self.turnAngle = getPosition.angleCheck(angle)
+        self.turnAngle = turretThreshold.angleCheck(angle)
 
     @state(first = True)
     def idling(self):
@@ -28,16 +28,16 @@ class turretTurn:
     def setSpeed(self, angle):
         self.pos = self.turretMotor.getEncoder().getPosition()
         if self.pos < (self.setAngle + angle) or self.pos > (self.setAngle - angle):
-            getPosition.runTurret(0.5)
+            turretThreshold.runTurret(0.5)
         else:
-            getPosition.runTurret(1)
+            turretThreshold.runTurret(1)
 
     @state
     def turn(self, angle):
         self.setSpeed()
-        getPosition.execute()
+        turretThreshold.execute()
         if self.pos < (self.turnAngle + self.tolerance) and self.pos > (self.turnAngle - self.tolerance):
             self.stop()
 
     def stop(self):
-        getPosition.stopTurret()
+        turretThreshold.stopTurret()
