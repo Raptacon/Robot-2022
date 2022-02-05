6da58c95c7850072f9e9705812c10fcc98130e49
DarthCooper <LoganBarber1@outlook.com>
2022-02-04 19:23:44 -0700

    Switched all of the shootermoter1s with turretMotor

--- components/turretThreashold.py
+++ components/turretThreashold.py
@@ -1,20 +1,18 @@
 import navx
 from magicbot import tunable, feedback, StateMachine, state
 from components.turnToAngle import TurnToAngle
-from components.shooterMotors import ShooterMotors
 class getPosition(navx):
     compatString = ["doof", "greenChassis"]
     turnToangle: TurnToAngle
     navx = navx._navx.AHRS.create_spi()
     Deadzones = [[-45, -405] [360, 360] [45, 405]]
-    shooterMotor: ShooterMotors
     motors_turret: dict
 
     def setup(self):
         self.turretMotor = self.motors_turret["turretMotor"]
 
     def getOriginalheading(self):
-        self.shooterMotor.shooterMotor1.getencoder().getPosition()
+        self.turretMotor.getEncoder().getPosition()
 
 
     def stop(self):
@@ -24,7 +22,7 @@ class getPosition(navx):
 
     def checkClamp(self):
         for leftLim, right in self.Deadzones:
-            if self.shooterMotor.shooterMotor1.getencoder().getPosition() < leftLim or self.shooterMotor.shooterMotor1.getencoder().getPosition() > right:
+            if self.turretMotor.getEncoder().getPosition() < leftLim or self.turretMotor.getEncoder().getPosition() > right:
                 self.stop()
 
 
