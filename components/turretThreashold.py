38d6297daf0e0a5c70687d8d77ae22ee683e0ab1
IanDKsemantic <iankeitlen@gmail.com>
2022-02-04 18:30:35 -0700

    added definitions for stopping and checking current heading

--- components/turretThreashold.py
+++ components/turretThreashold.py
@@ -1,11 +1,11 @@
 import navx
 from magicbot import tunable, feedback, StateMachine, state
 from components.turnToAngle import TurnToAngle
-
 class getPosition(navx):
     compatString = ["doof", "greenChassis"]
     turnToangle: TurnToAngle
     navx = navx._navx.AHRS.create_spi()
+
 
     def getOriginalheading(self):
         self.originalHeading = self.navx.getFusedHeading()
@@ -14,3 +14,11 @@ class getPosition(navx):
         self.running = False
         self.starting = False
         #self.done()
+
+    def getCurrentheading(self):
+        pass
+
+
+    def checkClamp(self):
+        if abs(self.navx.getFusedHeading() - self.nextHeading) < self.tolerance:
+            self.stop()
