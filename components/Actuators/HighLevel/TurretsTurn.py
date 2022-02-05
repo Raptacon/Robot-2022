bef760ce65fbba09230609943366eab31b5da4d4
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2022-02-05 13:12:21 -0700

    Implemented speed sections

--- components/Actuators/HighLevel/TurretsTurn.py
+++ components/Actuators/HighLevel/TurretsTurn.py
@@ -1,27 +1,17 @@
 from magicbot import StateMachine, state, tunable
 from components.Actuators.LowLevel.turretThreshold import TurretThreshold
+from components.SoftwareControl.speedSections import SpeedSections
 
 class turretTurn(StateMachine):
     compatString = ["doof", "greenChassis"]
     motors_turret: dict
     turretThreshold: TurretThreshold
+    speedSections: SpeedSections
     turnAngle = 0
     tolerance = tunable(0.5)
-    values = [
-             [20, .15], # The first value is the limit, so it will
-             [45, .2],  # use the included speed if the distance is
-             [90, .25],# under this value and above the last.
-             [180, .3],
-             ["End", .4]
-             ]  # The array must end with "End" - this will be the value used
-    # if the target is really far away.
-
-    def on_enable(self):
-        #sets up position and connects motors
-        self.turretMotor = self.motors_turret["turretMotor"]
 
     def setAngle(self, angle):
-        #gets angle turret is turning to
+        """gets angle turret is turning to"""
         self.turnAngle = self.turretThreshold.angleCheck(angle)
 
     @state(first = True)
@@ -32,18 +22,18 @@ class turretTurn(StateMachine):
         else:
             self.next_state("idling")
 
-    def setSpeed(self, angle):
-        #Sets speed of turret based on what angle we are turning to
-        self.pos = self.turretMotor.getEncoder().getPosition()
-        for distTotargetAngle, speed in self.values:
-            if (distTotargetAngle == "End"
-                or self.pos < distTotargetAngle):
-                self.turretThreshold.setTurretspeed(speed)
-                break
+    def setSpeed(self):
+        """
+        Sets speed of turret based on what angle we are turning to
+        """
+        speed = self.speedSections.getSpeed(self.turretThreshold.getPosition(), "TurretsTurn")
+        self.turretThreshold.setTurretspeed(speed)
 
     @state
     def turn(self, angle):
-        #Starts turning process, if in tolerance it will stop
+        """
+        Starts turning process, if in tolerance it will stop
+        """
         self.setSpeed()
         if self.pos < (self.turnAngle + self.tolerance) and self.pos > (self.turnAngle - self.tolerance):
             self.stop()
@@ -51,6 +41,6 @@ class turretTurn(StateMachine):
             self.next_state("turn")
 
     def stop(self):
-        #stops turret
+        """stops turret"""
         self.next_state("idling")
         self.turretThreshold.stopTurret()
