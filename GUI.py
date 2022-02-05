1f0310d3fcce6a0dd2b43fe9149be9712c374f4f
Leif <leifcreer@gmail.com>
2022-02-05 12:01:48 -0800

    triangle and circles work without resize

--- GUI.py
+++ GUI.py
@@ -35,9 +35,8 @@ class window(tk.Tk):
 class Calc_circle(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Canvas(self)
-        label.pack(pady=10,padx=10)
-        print(controller)
+        self.label = tk.Canvas(self)
+        self.label.pack(fill = 'both',expand= True)
         x = 0
         y = 0
         numball = 5
@@ -46,13 +45,22 @@ class Calc_circle(tk.Frame):
             ball += 1
             x += 20
             y += 20
-            self.create_circle(x, y, 20, label)
+            self.create_circle(x, y, 20, self.label)
     def create_circle(self,x, y, r, canvasName): #center coordinates, radius
         x0 = x - r
         y0 = y - r
         x1 = x + r
         y1 = y + r
         canvasName.create_oval(x0, y0, x1, y1, fill='blue')
+def calc_tri(canvasNametri,root):
+    FOV = 81.3
+    x1 = root.winfo_width()
+    y1 = root.winfo_height()/2
+    x2 = (math.tan((180 - FOV)/2) * y1)
+    y2 = 0
+    x3 = x2
+    y3 = y1 *2
+    canvasNametri.create_polygon(x1,y1, x2,y2, x3,y3)
 class calc_triagle(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
@@ -68,5 +76,6 @@ class calc_triagle(tk.Frame):
         tri.create_polygon(x1,y1, x2,y2, x3,y3)
 
 Window = window()
-
+Window.geometry("400x300")
+Window.after(1, calc_tri, Window.frames[Calc_circle].label, Window)
 Window.mainloop()
