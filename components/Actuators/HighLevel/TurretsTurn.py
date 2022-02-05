fe1dd047bf8816d5e2bf067b0c697a0aba804973
IanDKsemantic <iankeitlen@gmail.com>
2022-02-05 09:44:02 -0700

    added get Position from turret threashold

--- components/Actuators/HighLevel/TurretsTurn.py
+++ components/Actuators/HighLevel/TurretsTurn.py
@@ -1,13 +1,15 @@
 from magicbot import state_machine, state, timed_state
+from components.Actuators.LowLevel.turretThreshold import getPosition
 
 class turretTurn:
     compatString = ["doof", "greenChassis"]
     motors_turret: dict
-    
+    GetPosition: getPosition
 
 
     def on_enable(self):
         self.turretMotor = self.motors_turret["turretMotor"]
+        self.GetPosition.setup()
         self.turret = False
 
 
