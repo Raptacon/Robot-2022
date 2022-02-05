981ba8cffdd1d1a14797a3073f66e285c1013f8c
IanDKsemantic <iankeitlen@gmail.com>
2022-02-04 18:14:27 -0700

    added stop function

--- components/turretThreashold.py
+++ components/turretThreashold.py
@@ -1,9 +1,16 @@
 import navx
 from magicbot import tunable, feedback, StateMachine, state
+from components.turnToAngle import TurnToAngle
 
 class getPosition(navx):
     compatString = ["doof", "greenChassis"]
+    turnToangle: TurnToAngle
     navx = navx._navx.AHRS.create_spi()
 
     def getOriginalheading(self):
         self.originalHeading = self.navx.getFusedHeading()
+
+    def stop(self):
+        self.running = False
+        self.starting = False
+        #self.done()
