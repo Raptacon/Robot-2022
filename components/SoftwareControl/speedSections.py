482fc54a4c1ba7c9a077ef65bac8345db04069ff
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2022-02-05 11:22:40 -0700

    probably doesn't work

--- components/SoftwareControl/speedSections.py
+++ components/SoftwareControl/speedSections.py
@@ -1,4 +1,5 @@
 import logging as log
+from wpimath.controller import PIDController
 
 def speedFactory(descp):
     return descp
@@ -8,6 +9,11 @@ class SpeedSections():
 
     def setup(self):
         self.speedSections = self.configuredValues_speedSections
+        self.PIDs = self.speedSections["PID"]
+        self.PIDControllers = []
+        for PIDset in self.PIDs.keys():
+            self.PIDControllers[PIDset] = PIDController()
+
 
     def getSpeedSection(self, component:str):
         """
@@ -23,16 +29,27 @@ class SpeedSections():
             log.error(component+"'s speed sections aren't present in current data")
             return
 
+    def getPID(self, component:str):
+        if component in self.PIDs:
+            return self.PIDs[component]
+        elif "None" in self.PIDs:
+            log.error("Current robot does not have speed sections PID defined")
+            return
+        else:
+            log.error(component+"'s PIDs aren't present in current data")
+            return
+
     def getSpeed(self, offset, component:str):
         """
+        Returns a speed for you based on an offset
+        and your component.
         """
         section = self.getSpeedSection(component)
         for dist, speed in section:
             if (dist == "End"
                 or offset < dist):
                 if speed == "PID":
-                    return NotImplementedError
-                    # Sorry
+                    return 0
                 return speed
 
     def execute(self):
