104af477c500d9fe533fb26ce907ea3ad18c805a
DarthCooper <LoganBarber1@outlook.com>
2022-02-05 10:16:43 -0700

    Set up idling state

--- components/Actuators/HighLevel/TurretsTurn.py
+++ components/Actuators/HighLevel/TurretsTurn.py
@@ -5,12 +5,19 @@ class turretTurn:
     compatString = ["doof", "greenChassis"]
     motors_turret: dict
     GetPosition: getPosition
-
+    turnAngle = 0
 
     def on_enable(self):
         self.turretMotor = self.motors_turret["turretMotor"]
         self.GetPosition.setup()
 
     def setAngle(self, angle):
-        getPosition.angleCheck(angle)
+        self.turnAngle = getPosition.angleCheck(angle)
 
+    @state(first = True)
+    def idling(self):
+        """Stays in this state until started"""
+        if self.turnAngle != 0:
+            self.next_state("")
+        else:
+            self.next_state("idling")
