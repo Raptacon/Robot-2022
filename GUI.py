2d6891b5df14c0e466255b617f05c2ebb98875aa
Leif <leifcreer@gmail.com>
2022-02-09 20:03:46 -0800

    got triagle to scale and circles to a one to one map for now

--- GUI.py
+++ GUI.py
@@ -9,10 +9,10 @@ import math
 limeTable = networktable.getTable("limelight")
 
 class window(tk.Tk):
+    width  = round((math.tan(math.radians((180 - 81.3)/2)) * 200))
     def __init__(self):
         tk.Tk.__init__(self)
         myCanvas = tk.Frame(self, background= 'white')
-        myCanvas.config(highlightthickness= 5, highlightbackground= "red")
         myCanvas.pack(fill = "both", expand = True)
         
         myCanvas.grid_rowconfigure(0, weight=1)
@@ -37,25 +37,27 @@ class Calc_circle(tk.Frame):
         tk.Frame.__init__(self,parent)
         self.label = tk.Canvas(self)
         self.label.pack(fill = 'both',expand= True)
-        position = [[5,0.5],[10,1],[15,-11],[54,0]]
+        position = [[5,40.65],[10,40.65],[15,0],[54,40.65]]
         numball = len(position)
         HightOffSet = 2
         ball = 0
-        self.calc_tri(self.label)
+        winx = self.calc_tri(self.label)
         #self.label.tkraise()
-        print(position[ball][0])
         while(ball < numball):
             if position[ball][0] <= 10:
                 hyp = math.sqrt(position[ball][0] ** 2 - HightOffSet ** 2)
-                xft = hyp * math.cos(position[ball][1])
-                yft = hyp * math.sin(position[ball][1])
-                x = 400 - (xft * 40)
-                y = 150 + (yft * 15)
+                xft = hyp * math.cos(math.radians(position[ball][1]))
+                yft = hyp * math.sin(math.radians(position[ball][1]))
+                x = window.width - (xft * (window.width/10))
+                y = 200 + (yft * 20)
+                print(y,yft)
                 self.create_circle(x, y, 5, self.label)
             else:
+                hyp = math.sqrt(position[ball][0] ** 2 - HightOffSet ** 2)
                 x = 0
-                yft = hyp * math.sin(position[ball][1])
-                y = 150 + (yft * 15)
+                yft = hyp * math.sin(math.radians(position[ball][1]))
+                y = 200 + (yft * 20)
+                print(y,yft)
                 self.create_circle(x, y, 5, self.label)
             ball += 1
     def create_circle(self,x, y, r, canvasName): #center coordinates, radius
@@ -66,15 +68,16 @@ class Calc_circle(tk.Frame):
         canvasName.create_oval(x0, y0, x1, y1, fill='blue')
     def calc_tri(self,canvasNametri):
         FOV = 81.3
-        x1 = 400
-        y1 = 300/2
-        x2 = (math.tan((180 - FOV)/2) * y1)
+        x1 = window.width
+        y1 = 400/2
+        x2 = 0
         y2 = 0
         x3 = x2
-        y3 = 300
+        y3 = 400
         canvasNametri.create_polygon(x1,y1, x2,y2, x3,y3)
+        return x2
 
 Window = window()
-Window.geometry('400x300+0+0')
+Window.geometry(str(Window.width)+'x400+0+0')
 Window.resizable(0,0)
 Window.mainloop()
