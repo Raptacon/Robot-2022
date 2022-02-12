c9aa917ff5364bbd8cf6eb27b89993596bc988b3
IanDKsemantic <iankeitlen@gmail.com>
2022-02-12 10:58:13 -0700

    added state machine and base for first method

--- components/Actuators/HighLevel/TurretCalibrate.py
+++ components/Actuators/HighLevel/TurretCalibrate.py
@@ -1,4 +1,10 @@
+from magicbot import state_machine, state
+
+class CalibrateTurret(state_machine):
+    compatString = ["doof", "greenChassis", "newBot"]
+
+    @state(first = True)
+    def findLeftDeadzone(self):
+        pass
 
 
-class CalibrateTurret:
-    compatString = ["doof", "greenChassis"]
