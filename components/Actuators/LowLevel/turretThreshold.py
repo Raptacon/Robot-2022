7924543f8916d123ebff1737b4e04ef282896b03
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2022-02-12 10:18:29 -0700

    cleaned up

--- components/Actuators/LowLevel/turretThreshold.py
+++ components/Actuators/LowLevel/turretThreshold.py
@@ -30,24 +30,6 @@ class TurretThreshold:
         """
         Checks if desired angle is within the deadzone.Then, returns closest point to the angle it can reach.
         """
-        # for leftLim, rightLim in self.Deadzones:
-        #     if angle > leftLim and angle < rightLim:
-        #         if 270 < angle < 360:
-        #             if rightLim < (angle -180) < leftLim:
-        #                 angle = leftLim
-        #         elif 180 < angle < 269.9:
-        #             if rightLim < (angle - 90) < leftLim:
-        #                 angle = leftLim
-        #         elif 0 < angle < 89.9:
-        #             if rightLim < (angle + 90) < leftLim:
-        #                 angle = rightLim
-
-
-        #         if abs(leftLim - angle) < abs(rightLim - angle):
-        #             return leftLim
-        #         else:
-        #             return rightLim
-
         for lLim, rLim in self.Deadzones:
             # If we're jumping the deadzone in either direction
             # or the angle is inside of the deadzone
@@ -64,20 +46,6 @@ class TurretThreshold:
                 if rLim <= self.pos:
                     return rLim
         return angle
-
-        #     # Check paths
-        #     if leftLim > self.pos and leftLim < angle:
-        #         return leftLim
-        #     elif rightLim < self.pos and rightLim > angle:
-        #         return rightLim
-        # return angle
-
-    # def DetermineShortestPath(self, angle, leftLim, rightLim):
-    #     self.DegreeToAngle = self.pos
-    #     while self.DegreeToAngle != angle:
-    #         self.DegreeToAngle += 0.1
-    #         if self.DegreeToAngle > leftLim and self.DegreeToAngle < rightLim:
-    #             return self.DegreeToAngle
 
     @feedback
     def getPosition(self):
