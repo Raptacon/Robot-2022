be521fef7134193e56b21fd2dbf774fe77ef9c4d
ManiacMoustache <cjkesler@unionvotes.net>
2022-02-12 11:49:55 -0700

    Added right limit

--- components/Actuators/HighLevel/TurretCalibrate.py
+++ components/Actuators/HighLevel/TurretCalibrate.py
@@ -1,12 +1,19 @@
-from magicbot import state_machine, state
 
+from magicbot import state_machine, state
+from components.Actuators.AutonomousControl.turretTurn import TurretTurn
 class CalibrateTurret(state_machine):
     compatString = ["doof", "greenChassis", "newBot"]
     clicked = False
     #a dummy variable until we find something else
+    turretTurn: TurretTurn
 
     @state(first = True)
     def findRightdeadzone(self):
-        pass
+        while self.clicked == False:
+            self.turretTurn()
+            if self.clicked == True:
+                self.limitR = self.turretTurn.setup()
+
+
 
 
