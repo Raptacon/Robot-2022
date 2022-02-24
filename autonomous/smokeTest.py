c9d728d3a024a6213b099f12acee5ff798e9df5d
hell0w0rld2 <43834630+hell0w0rld2@users.noreply.github.com>
2022-02-23 18:24:44 -0700

    Final changes

--- autonomous/smokeTest.py
+++ autonomous/smokeTest.py
@@ -34,7 +34,6 @@ class SmokeTest(AutonomousStateMachine):
 
     @state(first = True)
     def driveSetup(self):
-        self.driveTrain.setup()
         self.driveTrain.resetDistTraveled()
         self.next_state("drive")
 
