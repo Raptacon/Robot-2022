e108723da9c3d65a094df6ec8a75b241ec891f10
ManiacMoustache <cjkesler@unionvotes.net>
2022-02-28 20:55:48 -0700

    Added second shooting sequence

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -1,4 +1,5 @@
 from magicbot import AutonomousStateMachine, tunable, timed_state, state
+from components.Input.ballCounter import Ball, BallCounter
 from components.Actuators.LowLevel.driveTrain import DriveTrain
 from components.Actuators.HighLevel.shooterLogic import ShooterLogic
 from components.Actuators.LowLevel.pneumatics import Pneumatics
@@ -23,7 +24,12 @@ class Autonomous(AutonomousStateMachine):
     turretCalibrate: CalibrateTurret
     turretTurn: TurretTurn
     turretThreshold: TurretThreshold
+    ballCounter: BallCounter
     drive_speed = tunable(.25)
+
+    allianceColor: str
+
+    afterShootState = "calibrateTurret_move"
 
     moveComplete = False
     currentMove = 0
@@ -66,7 +72,7 @@ class Autonomous(AutonomousStateMachine):
         self.shooter.startShooting()
         self.next_state('shooter_wait')
 
-    @timed_state(duration = shootTime, next_state="calibrateTurret_move")
+    @timed_state(duration = shootTime, next_state=afterShootState)
     def shooter_wait(self):
         """Waits for shooter to finish, then next state"""
         pass
@@ -87,7 +93,8 @@ class Autonomous(AutonomousStateMachine):
             elif move[0] == "drive":
                 self.goToDist.setTargetDist(move[1])
                 self.goToDist.engage()
-                if not self.goToDist.running and self.goToDistPrevRunning:
+                if ((not self.goToDist.running and self.goToDistPrevRunning) or
+                self.ballCounter.getBallCount()[0] != None):
                     self.currentMove += 1
 
             if self.currentMove == len(self.moveSequence):
@@ -95,24 +102,43 @@ class Autonomous(AutonomousStateMachine):
 
         self.turretCalibrate.engage()
         self.next_state("calibrateTurret_move")
-        if self.turretThreshold.calibrated == True and self.moveComplete:
+        if self.turretThreshold.calibrated == True:
             self.turretTurn.done()
             self.turretThreshold.setTurretspeed(0)
+
+        if self.turretThreshold.calibrated == True and self.moveComplete:
             self.next_state("finishCalibration")
 
     @state
     def finishCalibration(self):
         self.turretThreshold.setTurretspeed(0)
-        self.next_state("stop")
+        self.next_state("engage_Shooter2")
 
-    @timed_state(duration = time, next_state = 'stop')
-    def drive(self):
-        """Drives the bot backwards for a time"""
-        self.shooter.doneShooting()
-        self.driveTrain.setTank(self.drive_speed, self.drive_speed)
+    @state
+    def engage_Shooter2(self):
+        ball1 = self.ballCounter.getBallCount[0]
+        self.afterShootState = "stop"
+
+        # We don't need the second condition, but it sounds fun
+        if ball1 != None and ball1.getColor() == self.allianceColor:
+            self.shooter.engage()
+            self.shooter.startShooting()
+            self.next_state('shooter_wait')
+        elif ball1.getColor() != self.allianceColor:
+            self.shooter.autoShootingSpeed1 = 1000
+            self.shooter.autoShootingSpeed2 = 500
+            self.shooter.engage()
+            self.shooter.startShooting()
+            self.next_state('shooter_wait')
+        else:
+            self.next_state("stop")
+
 
     @state(must_finish = True)
     def stop(self):
         """Stops driving bot"""
         self.driveTrain.setTank(0, 0)
+        self.turnToAngle.done()
+        self.goToDist.done()
+        self.turretTurn.done()
         self.done()
