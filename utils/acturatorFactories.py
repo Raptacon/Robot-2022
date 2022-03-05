e5af2a3270b18354dc1f318fa1e30e611de20045
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2022-03-05 13:56:58 -0700

    Pneumatics Fix (#74)
    * needed to specify CAN ID
    
    * test code
    
    * This is incredibly stupid

--- utils/acturatorFactories.py
+++ utils/acturatorFactories.py
@@ -13,7 +13,7 @@ def compressorFactory(descp):
     """
     try:
         if descp["type"] == "compressor":
-            return wpilib.Compressor(wpilib.PneumaticsModuleType.CTREPCM)
+            return wpilib.Compressor(descp["pcm"], wpilib.PneumaticsModuleType.CTREPCM)
     except Exception as e:
         logging.error("Failed to create compressor for %s err %s", descp, e)
     return None
@@ -23,19 +23,19 @@ def solenoidFactory(descp):
     """
     Creates single and double solenoids devices from descp
     """
-    try:
-        pcm = 0
-        if pcm in descp:
-            pcm = descp["pcm"]
-        if descp["type"] == "solenoid":
-            return wpilib.Solenoid(pcm, descp["channel"])
-        if descp["type"] == "doubleSolenoid":
-            solenoid = wpilib.DoubleSolenoid(pcm, descp["channel"]["forward"], descp["channel"]["reverse"])
-            if "default" in descp:
-                value = {"kOff":0, "kForward":1, "kReverse":2}[descp["default"]]
-                solenoid.set(wpilib.DoubleSolenoid.Value(value))
-            return solenoid
-    except Exception as e:
-        logging.error("Failed to create solenoid %s. Err %s", descp, e)
+    # try:
+    pcm = 0
+    if 'pcm' in descp:
+        pcm = descp["pcm"]
+    if descp["type"] == "solenoid":
+        return wpilib.Solenoid(pcm, wpilib.PneumaticsModuleType.CTREPCM, descp["channel"])
+    if descp["type"] == "doubleSolenoid":
+        solenoid = wpilib.DoubleSolenoid(pcm, wpilib.PneumaticsModuleType.CTREPCM, descp["channel"]["forward"], descp["channel"]["reverse"])
+        if "default" in descp:
+            value = {"kOff":0, "kForward":1, "kReverse":2}[descp["default"]]
+            solenoid.set(wpilib.DoubleSolenoid.Value(value))
+        return solenoid
+    # except Exception as e:
+    #     logging.error("Failed to create solenoid %s. Err %s", descp, e)
 
     return None
