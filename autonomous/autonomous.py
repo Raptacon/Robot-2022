ef4559468d29200bddf62c23575b60af78ed8fb2
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2022-03-11 10:55:56 -0700

    cleared variables

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -54,6 +54,14 @@ class Autonomous(AutonomousStateMachine):
                     [["turn", -59.993],
                     ["drive", 5.62733*12]]]
 
+    def setup(self):
+        self.moveComplete = False
+        self.currentMove = 0
+        self.robotPosition = tunable(1)
+        self.turnToAnglePrevRunning = False
+        self.goToDistPrevRunning = False
+        self.turretTurnPrev = True
+
     @state(first=True)
     def init(self):
         self.driveTrain.resetDistTraveled()
