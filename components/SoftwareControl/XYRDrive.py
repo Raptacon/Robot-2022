5465359fe34cd9f4996cdc24334e5c9f862b58a3
Caiden <cjkesler@unionvotes.net>
2022-06-01 18:49:35 -0700

    added logic to convert tank to motor outputs

--- components/SoftwareControl/XYRDrive.py
+++ components/SoftwareControl/XYRDrive.py
@@ -1,6 +1,5 @@
-from SoftwareControl import AxesXYR
-from components.Actuators.LowLevel.driveTrain import ControlMode
-
+from components.SoftwareControl.AxesXYR import AxesXYR
+from components.Actuators.HighLevel.driveTrainHandler import DriveTrainHandler
 class XYRDrive:
     mode = 0
     transformDict = {"Tank", "Arcade", "Swerve"}
@@ -13,8 +12,25 @@ class XYRDrive:
             swerve
             
 class tank:
-    pass
-    
+    driveTrainHandler: DriveTrainHandler
+    lmotor = 0
+    rmotor = 0
+    def tankdrive(self,x,y,r):
+        if y >= 0:
+        if r >= 0:  # I quadrant
+            self.lmotor = y
+            self.rmotor = r
+        else:            # II quadrant
+            self.lmotor = r
+            self.rmotor = y
+    else:
+        if r >= 0:  # IV quadrant
+            self.lmotor = 1 + y
+            self.rmotor = r
+        else:            # III quadrant
+            self.lmotor = r
+            self.rmotor = 1 + y
+
 class arcade:
     pass
     
