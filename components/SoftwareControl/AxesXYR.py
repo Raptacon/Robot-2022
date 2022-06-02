796148878b29663e101a7aecdac996cb9d0f0e41
Caiden <cjkesler@unionvotes.net>
2022-06-01 17:09:20 -0700

    Added vectorXYR

--- components/SoftwareControl/AxesXYR.py
+++ components/SoftwareControl/AxesXYR.py
@@ -1,4 +1,13 @@
+from re import X
+from tkinter import Y
 from utils.XYRVector import XYRVector
 from robotMap import XboxMap
 
 class AxesXYR:
+
+X = 0
+Y = 0
+R = 0
+vectorXYR = [X],[Y],[R]
+
+def Trasformations(imp1,imp2,imp3,imp4,self):
