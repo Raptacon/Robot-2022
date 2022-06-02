b3d24bb4aac7b2c31dff232916a9a15f08c49fce
Caiden <cjkesler@unionvotes.net>
2022-06-01 18:17:53 -0600

    transform system set up

--- components/SoftwareControl/AxesXYR.py
+++ components/SoftwareControl/AxesXYR.py
@@ -3,7 +3,18 @@ from tkinter import Y
 from utils.XYRVector import XYRVector
 from robotMap import XboxMap
 
+class transform:
+    pass
+class transformTank(transform):
+    pass
+
+class transformArcade(transform):
+    pass
+
+class transformSwerve(transform):
+    pass
 class AxesXYR:
+<<<<<<< Updated upstream
 
 X = 0
 Y = 0
@@ -11,3 +22,12 @@ R = 0
 vectorXYR = [X],[Y],[R]
 
 def Trasformations(imp1,imp2,imp3,imp4,self):
+=======
+    transformDict = {"tank":transformTank, "arcade":transformArcade, "swerve":transformSwerve}
+
+    def transform(self, transformKey:str, Axes: list):
+
+        if transformKey in self.transformDict.keys():
+            transformer = self.transformDict[transformKey]
+            return transformer.transform(Axes)
+>>>>>>> Stashed changes
