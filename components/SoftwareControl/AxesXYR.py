87141e99712f696f1761026d914e038d638a28e1
Caiden <cjkesler@unionvotes.net>
2022-06-01 18:22:00 -0600

    Added examples

--- components/SoftwareControl/AxesXYR.py
+++ components/SoftwareControl/AxesXYR.py
@@ -1,10 +1,9 @@
-from re import X
-from tkinter import Y
 from utils.XYRVector import XYRVector
 from robotMap import XboxMap
 
 class transform:
-    pass
+    def transform(self, Axes):
+        return XYRVector(0, 0, 0)
 class transformTank(transform):
     pass
 
@@ -14,20 +13,10 @@ class transformArcade(transform):
 class transformSwerve(transform):
     pass
 class AxesXYR:
-<<<<<<< Updated upstream
-
-X = 0
-Y = 0
-R = 0
-vectorXYR = [X],[Y],[R]
-
-def Trasformations(imp1,imp2,imp3,imp4,self):
-=======
     transformDict = {"tank":transformTank, "arcade":transformArcade, "swerve":transformSwerve}
 
-    def transform(self, transformKey:str, Axes: list):
+    def transform(self, transformKey:str, Axes:list):
 
         if transformKey in self.transformDict.keys():
             transformer = self.transformDict[transformKey]
             return transformer.transform(Axes)
->>>>>>> Stashed changes
