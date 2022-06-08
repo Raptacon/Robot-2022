0a6b8f9c3fb896c6779c679969098660574b4ffa
Caiden <cjkesler@unionvotes.net>
2022-06-08 16:36:32 -0700

    Added logic to the arcade swerve and tank classe

--- components/SoftwareControl/XYRDrive.py
+++ components/SoftwareControl/XYRDrive.py
@@ -1,22 +1,14 @@
 from components.SoftwareControl.AxesXYR import AxesXYR
 from components.Actuators.HighLevel.driveTrainHandler import DriveTrainHandler
-class XYRDrive:
-    mode = 0
-    transformDict = {"Tank", "Arcade", "Swerve"}
-    def xyrdrive(self):
-        if self.transformDict[self.mode] == 'Tank':
-            tank
-        elif self.transformDict[self.mode] == 'Arcade':
-            arcade
-        elif self.transformDict[self.mode] == 'Swerve':
-            swerve
+import math
+
             
 class tank:
     driveTrainHandler: DriveTrainHandler
     lmotor = 0
     rmotor = 0
     def tankdrive(self,x,y,r):
-        if y >= 0:
+    if y >= 0:
         if r >= 0:  # I quadrant
             self.lmotor = y
             self.rmotor = r
@@ -32,7 +24,52 @@ class tank:
             self.rmotor = 1 + y
 
 class arcade:
-    pass
+    driveTrainHandler: DriveTrainHandler
+    lmotor = 0
+    rmotor = 0
+    def tankdrive(self,x,y,r):
+    if y >= 0:
+        if r >= 0:  # I quadrant
+            self.lmotor = y
+            self.rmotor = r
+        else:            # II quadrant
+            self.lmotor = r
+            self.rmotor = y
+    else:
+        if r >= 0:  # IV quadrant
+            self.lmotor = 1 + y
+            self.rmotor = r
+        else:            # III quadrant
+            self.lmotor = r
+            self.rmotor = 1 + y
     
 class swerve:
-    pass
+    L = 30
+    W = 30
+
+    def rec (self, x, y, r):
+        ratio = math.sqrt ((self.L * self.L) + (self.W * self.W))
+        y *= -1
+
+        a = x - r * (self.L / ratio)
+        b = x + r * (self.L / ratio)
+        c = y - r * (self.W / ratio)
+        d = y + r * (self.W / ratio)
+
+        backRightSpeed = math.sqrt ((a * a) + (d * d))
+        backLeftSpeed = math.sqrt ((a * a) + (c * c))
+        frontRightSpeed = math.sqrt ((b * b) + (d * d))
+        frontLeftSpeed = math.sqrt ((b * b) + (c * c))
+
+        backRightAngle = math.atan2 (a, d) / math.pi
+        backLeftAngle = math.atan2 (a, c) / math.pi
+        frontRightAngle = math.atan2 (b, d) / math.pi
+        frontLeftAngle = math.atan2 (b, c) / math.pi
+
+class XYRDrive:
+    mode = 0
+    transformDict = {"Tank":tank, "Arcade":arcade, "Swerve":swerve}
+
+    def xyrdrive(self, transformKey:str):
+        if transformKey in self.transformDict.keys():
+            transformer = self.transformDict[transformKey]
