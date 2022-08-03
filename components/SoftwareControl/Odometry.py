839800b3247898f55b8d78923b175490aa795dfc
Caiden <cjkesler@unionvotes.net>
2022-08-03 17:27:04 -0600

    Basic layout of methods in class

---
+++ components/SoftwareControl/Odometry.py
@@ -0,0 +1,13 @@
+
+class Odometry():
+    IntialPostion = 0
+    GyroAngle = 0
+    Kinamatics = 0
+    
+    def getPosition():
+        
+    def resetPosition():
+        
+    def Update():
+
+    def UpdateWithTime():
